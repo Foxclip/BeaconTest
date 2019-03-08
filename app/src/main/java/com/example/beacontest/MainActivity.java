389632faf36fae5f1c47d93b0973dc748b8aa9af
Foxclip <destroyApp2@gmail.com>
2019-03-08 16:27:37 +1000

    Bluetooth check

--- app/src/main/java/com/example/beacontest/MainActivity.java
+++ app/src/main/java/com/example/beacontest/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.beacontest;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private BluetoothAdapter bluetoothAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //Initializing UI
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //checking if device supports BLE
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+        } else {
+            Toast.makeText(this, "BLE ok", Toast.LENGTH_SHORT).show();
+        }
+
+        //getting bluetooth adapter
+        final BluetoothManager bluetoothManager = (BluetoothManager)getSystemService(Context.BLUETOOTH_SERVICE);
+        bluetoothAdapter = bluetoothManager.getAdapter();
+
+        //checking if bluetooth is on
+        int REQUEST_ENABLE_BT = 1;
+        if(bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
     }
 }
